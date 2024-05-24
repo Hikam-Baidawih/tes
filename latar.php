72ea059bc700283bd179322365c7b5aebc879b90
Hikam-Baidawih <hikambaidawih@gmail.com>
2024-05-25 00:59:39 +0700

    tambahan makanan yang tidak disukai

--- latar.php
+++ latar.php
@@ -3,6 +3,7 @@
     $makanan = array("pisang", "duren", "pepaya");
     echo"nama saya adalah {$nama} <br>";
     echo"saya tidak suka {$makanan[0]}";
-    echo "saya juga tidak suka dengan {$makanan[1]}";
+    echo "saya juga tidak suka dengan {$makanan[1]} <br>";
+    echo "saya juga tidak suka dengan {$makanan[2]}";
 
 ?>
